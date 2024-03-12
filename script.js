e2ca2178a7af3d3324ec0abe243cb646a8fd1019
Rahul Kr Mishra <157671988+webbraindeveloper@users.noreply.github.com>
2024-03-12 10:42:13 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      selectable: true,
+      dateClick: function(info) {
+        document.getElementById('date').value = info.dateStr;
+      }
+    });
+    calendar.render();
+  });
+  
